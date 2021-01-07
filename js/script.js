cb797524d152d031e3ab6005d9ca9466e226cb0e
Vito Gentile <gentilevito1076@gmail.com>
2021-01-07 15:01:06 +0100

    Start

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// -Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+// -Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+// Consigli del giorno
+// 1. Scriviamo sempre in italiano i passaggi che vogliamo fare, usate i commenti per individuare gli ostacoli da superare e gli aspetti della logica più ostili
+// 2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+// 3. Se volete, sviluppate tutto nello stesso file html + js, o dividete in cartelle differenti. Insomma, fate come ve pare
+// ---------------------------------------------------------------------------------
